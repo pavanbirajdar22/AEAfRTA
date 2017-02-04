00e86094dd806b717b21f08ceb92e582a579a09e
pavanbirajdar22 <Pavan@Pavans-MacBook-Air.local>
2017-02-04 15:05:11 +0530

    Working encryption and decryption

--- src/XorDataNKey.java
+++ src/XorDataNKey.java
@@ -1,16 +1,18 @@
 public class XorDataNKey {
+	
 	public static byte[] XorDataWithKey(String data,byte[] key) {
-	    byte[] byteData = data.getBytes();
-	    byte[] xorData = new byte[128];
-	    for(int i=0;i<byteData.length;i++ ){
-	    	xorData[i] = (byte)(0xff & ((int)byteData[i]) ^ ((int)key[i]));
-	    }
-	    return xorData;
+		byte[] byteData = data.getBytes();
+		byte[] xorData = new byte[byteData.length];
+		for(int i=0;i<byteData.length;i++ ){
+			xorData[i] = (byte)(0xff & ((int)byteData[i]) ^ ((int)key[i]));
+		}
+		return xorData;
 	}
+	
 	public static byte[] XorDataWithKeyAtIndex(byte[] xorData,byte[] key,int[] indexes) {
-	    for(int i=0;i<xorData.length;i++ ){
-	    	xorData[i] = (byte)(0xff & ((int)xorData[i]) ^ ((int)key[indexes[i]]));
-	    }
-	    return xorData;
+		for(int i=0;i<xorData.length;i++ ){
+			xorData[i] = (byte)(0xff & ((int)xorData[i]) ^ ((int)key[indexes[i]]));
+		}
+		return xorData;
 	}
 }
