e106dc8ff38066e0bcaede305ca47a5e8c243365
sagandhi95 <sohamgandhi95@gmail.com>
2017-02-04 15:06:19 +0530

    Server Improvements

--- src/Server.java
+++ src/Server.java
@@ -1,10 +1,4 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.DataInputStream;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -12,41 +6,34 @@ public class Server{
 
 	private static Socket socket;
 	private static ServerSocket serverSocket; 
+	private static int port = 25000;
 	
 	private static int table[][];
 	private static byte[] key;
+	
 	private static byte[] xorData;
+	private static byte[] encryptedData;
+	private static String decryptedData;
 	
 	public static void main(String[] args){
 		
 		try{
-
-			int port = 25000;
+			
 			serverSocket = new ServerSocket(port);
-			System.out.println("Server Started and listening to the port 25000");
-
+			System.out.println("Server Online");
 			while(true){
 				
 				//Reading the message from the client
 				socket = serverSocket.accept();
-				
-				/*InputStream is = socket.getInputStream();
-				InputStreamReader isr = new InputStreamReader(is);
-				BufferedReader br = new BufferedReader(isr);
-				String key = br.readLine();
-				System.out.println("Message received from client is " + key);
-*/
-				
-				
+							
 				DataInputStream din = new DataInputStream(socket.getInputStream());
 				
-				int indexes[];
+				int indexes[] = null;
 				int length = din.readInt();                 
 				if(length > 0) {
 					indexes = new int[length];
 					for (int i = 0; i < indexes.length; ++i) {
 						indexes[i] = din.readInt();
-						System.out.print(indexes[i] + " ");
 					}
 						
 				}
@@ -56,25 +43,19 @@ public class Server{
 					key = new byte[length];
 				    din.readFully(key, 0, key.length); 
 				}
-				System.out.println(key);
+				//System.out.println(key);
 				
-				byte[] encryptedMessage = null;
+				encryptedData = null;
 				length = din.readInt();
 				if(length > 0) {
-					encryptedMessage = new byte[length];
-				    din.readFully(encryptedMessage, 0, encryptedMessage.length); 
+					encryptedData = new byte[length];
+				    din.readFully(encryptedData, 0, encryptedData.length); 
 				}
-				System.out.println(encryptedMessage);
+				//System.out.println(encryptedData);
 				
-				
-				//Sending the response back to the client.
-				/*String returnMessage = key;
-				OutputStream os = socket.getOutputStream();
-				OutputStreamWriter osw = new OutputStreamWriter(os);
-				BufferedWriter bw = new BufferedWriter(osw);
-				bw.write(returnMessage);
-				System.out.println("Message sent to the client is " + returnMessage);
-				bw.flush();*/
+				xorData = XorDataNKey.XorDataWithKeyAtIndex(encryptedData, key, indexes);
+				decryptedData = XorDataNKey.XorDataWithKey(xorData, key);
+				System.out.println(decryptedData);
 				
 			}
 		}
--- src/XorDataNKey.java
+++ src/XorDataNKey.java
@@ -1,12 +1,24 @@
 public class XorDataNKey {
+	
 	public static byte[] XorDataWithKey(String data,byte[] key) {
 	    byte[] byteData = data.getBytes();
-	    byte[] xorData = new byte[128];
-	    for(int i=0;i<byteData.length;i++ ){
+	    byte[] xorData = new byte[byteData.length];
+	    
+	    for(int i = 0; i < byteData.length; i++ ){
 	    	xorData[i] = (byte)(0xff & ((int)byteData[i]) ^ ((int)key[i]));
 	    }
 	    return xorData;
 	}
+	
+	public static String XorDataWithKey(byte[] byteData, byte[] key) {
+	    
+	    for(int i = 0; i < byteData.length;i++ ){
+	    	byteData[i] = (byte)(0xff & ((int)byteData[i]) ^ ((int)key[i]));
+	    }
+	    return new String(byteData);
+	}
+	
+	
 	public static byte[] XorDataWithKeyAtIndex(byte[] xorData,byte[] key,int[] indexes) {
 	    for(int i=0;i<xorData.length;i++ ){
 	    	xorData[i] = (byte)(0xff & ((int)xorData[i]) ^ ((int)key[indexes[i]]));
